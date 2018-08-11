73ef7dcec57f9dcb6bfdba899a8db85a561ca012
AntonAzarenko <ant-azarenko@mail.ru>
2018-08-11 16:42:09 +0300

    add authorization via database

--- src/main/java/com.azarenko/web/LoggedUser.java
+++ src/main/java/com.azarenko/web/LoggedUser.java
@@ -1,16 +1,29 @@
 package com.azarenko.web;
 
 import com.azarenko.model.Role;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Collections;
 import java.util.Set;
 
 public class LoggedUser {
     protected int id = 0;
+
     protected Set<Role> roles = Collections.singleton(Role.ROLE_USER);
+
     protected boolean enabled = false;
+
     private static LoggedUser LOGGED_USER = new LoggedUser();
 
+    public static LoggedUser safeGet(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth == null){
+            return null;
+        }
+        Object user = auth.getPrincipal();
+        return (user instanceof LoggedUser) ? (LoggedUser) user : null;
+    }
     public int getId() {
         return id;
     }
